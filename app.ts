35455eeae804e860b93031e8dc251b8a0188ebf8
joao salgueiro <jp-siqueira@outlook.com>
2025-03-11 19:21:49 -0300

    Adding some modules and Firebase Integration

--- app.ts
+++ app.ts
@@ -1,26 +1,25 @@
-const movieInput = document.getElementById("movie-input") as HTMLInputElement;
+// const movieInput = document.getElementById("movie-input") as HTMLInputElement;
+// const addMovieButton = document.getElementById("add-movie") as HTMLButtonElement;
+// const movieList = document.getElementById("movie-list") as HTMLUListElement;
 
-const addMovieButton = document.getElementById("add-movie") as HTMLButtonElement;
+// addMovieButton.addEventListener("click", () => {
+//     const movieName = movieInput.value.trim();
+//     if (movieName === "") return;
 
-const movieList = document.getElementById("movie-list") as HTMLUListElement;
-
-addMovieButton.addEventListener("click", () => {
-    const movieName = movieInput.value.trim();
-    if (movieName === "") return;
-
-    const listItem = document.createElement("li");
-    listItem.textContent = movieName;
+//     const listItem = document.createElement("li");
+//     listItem.textContent = movieName;
     
-    const watchButton = document.createElement("button");
-    watchButton.textContent = "Watched";
-    watchButton.addEventListener("click", () => {
-        listItem.classList.toggle("watched");
-    });
+//     const watchButton = document.createElement("button");
+//     watchButton.textContent = "Watched";
+//     watchButton.addEventListener("click", () => {
+//         listItem.classList.toggle("watched");
+//     });
 
-    listItem.appendChild(watchButton);
-    movieList.appendChild(listItem);
+//     listItem.appendChild(watchButton);
+//     movieList.appendChild(listItem);
  
-    movieInput.value = "";
+//     movieInput.value = "";
 
-    
-});
+
+// });
+
